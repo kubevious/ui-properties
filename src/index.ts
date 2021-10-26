6089d44f149e0fe20c9765f7ad7bb842ae48ba19
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-10-26 12:57:04 -0700

    exposed control resolver

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export { Properties } from './Properties';
 export { PropertiesContents } from './PropertiesContents';
 export { Summary } from './Summary';
 
+export { PROPS_CONTROL_RESOLVER } from './control-registry';
+
 export * from './Components/helpers'
