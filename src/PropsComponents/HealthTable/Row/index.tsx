0850b78fcb2b0fb6bdb5223ac94551ef174360e6
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-26 21:50:45 -0700

    build fix

--- src/PropsComponents/HealthTable/Row/index.tsx
+++ src/PropsComponents/HealthTable/Row/index.tsx
@@ -1,8 +1,6 @@
 import _ from 'the-lodash';
 import React, { FC } from 'react';
 
-import styles from './styles.module.css';
-
 import { HealthMetric } from '../types';
 
 export interface RowProps {
